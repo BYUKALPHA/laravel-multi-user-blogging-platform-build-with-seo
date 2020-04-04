bbcb0899f15311be0be307b3907ce69517f5c408
Alphonse <alphonso2009@gmail.com>
2020-04-04 22:27:25 +0300

    Storing Categories

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -7,7 +7,17 @@
     <div class="card card-default">
         <div class="card-header">Add category</div>
         <div class="card-body">
-            <form action="">
+            @if($errors->any())
+                <div class="alert alert-danger">
+                    @foreach($errors->all() as $error)
+                        <li class="list-group-item text-danger">
+                            {{$error}}
+                        </li>
+                        @endforeach
+                </div>
+                @endif
+            <form action="{{route('categories.store')}}" method="POST">
+                @csrf
                 <div class="form-group">
                     <label for="name" >Name</label>
                     <input type="text" class="form-control" name="name">
